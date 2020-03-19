3ab087f2f1ffacdd810f201c223dcead13e74261
João Lenon <lenonSec7@hotmail.com>
2020-03-18 22:07:17 -0300

    feat: ⚡ League

---
+++ ZedJS/src/classes/dto/League/index.ts
@@ -0,0 +1,4 @@
+export * from './league-item/league-item.dto'
+export * from './league-item/league-list.dto'
+export * from './summoner-league/miniseries.dto'
+export * from './summoner-league/summoner-league.dto'
---
+++ ZedJS/src/classes/enums/division.ts
@@ -0,0 +1,6 @@
+export const enum Divisions {
+  I = 'I',
+  II = 'II',
+  III = 'III',
+  IV = 'IV'
+}
