7fd2b68655248bed871b2faa5407a518c76cb000
João Lenon <lenonSec7@hotmail.com>
2020-03-18 22:47:32 -0300

    feat: ⚡ Summoner

---
+++ ZedJS/src/api/lol/Summoner/summoner.dto.ts
@@ -0,0 +1,9 @@
+export class SummonerV4DTO {
+  public readonly profileIconId: number
+  public readonly name: string
+  public readonly puuid: string
+  public readonly summonerLevel: number
+  public readonly revisionDate: number
+  public readonly id: string
+  public readonly accountId: string
+}
