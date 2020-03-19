4ed8a1762f2e53cd0f28cf5399271aa2d027e9bd
João Lenon <lenonSec7@hotmail.com>
2020-03-18 21:44:27 -0300

    feat: ⚙ Refactor

--- ZedJS/src/classes/dto/Status/lol-status-service-indicents.dto.ts
+++ ZedJS/src/classes/dto/Status/lol-status-service-indicents.dto.ts
@@ -1,22 +1,8 @@
 import { LolStatusServiceIncidentsUpdate } from './lol-status-service-incidents-updates.dto'
-/**
- * Lol Status service incident
- */
+
 export class LolStatusServiceStatusIncident {
-  /**
-   * Is Active
-   */
   active: boolean
-  /**
-   * Indicent creation date
-   */
   created_at: string
-  /**
-   * Indicent id
-   */
   id: number
-  /**
-   * Updates list
-   */
   updates: LolStatusServiceIncidentsUpdate
 }
