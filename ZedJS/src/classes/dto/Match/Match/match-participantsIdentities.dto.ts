2998bf2eaca66aa741c65112cefa1780a1118b8a
João Lenon <lenonSec7@hotmail.com>
2020-03-18 21:54:37 -0300

    feat: ⚙ Refactor

--- ZedJS/src/classes/dto/Match/Match/match-participantsIdentities.dto.ts
+++ ZedJS/src/classes/dto/Match/Match/match-participantsIdentities.dto.ts
@@ -1,12 +1,6 @@
 import { MatchParticipantsIdentitiesPlayerDto } from './match-participants-identities-player.dto'
 
 export class MatchParticipantsIdentitiesDto {
-  /**
-   * Player information
-   */
   player: MatchParticipantsIdentitiesPlayerDto
-  /**
-   * Participant id
-   */
   participantId: number
 }
--- ZedJS/src/classes/dto/Match/Match/match-teams-bans.dto.ts
+++ ZedJS/src/classes/dto/Match/Match/match-teams-bans.dto.ts
@@ -1,14 +1,5 @@
 export class BannedChampion {
-  /**
-   * Turn during which the champion was banned.
-   */
   pickTurn: number
-  /**
-   * Banned championId.
-   */
   championId: number
-  /**
-   * The ID of the team that banned the champion
-   */
   teamId?: number
 }
