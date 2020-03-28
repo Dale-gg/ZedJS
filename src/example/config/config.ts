59cf585d162dca1338810e5ea4332ffeab97cf20
João Lenon <lenonsec7@gmail.com>
2020-03-28 17:40:45 -0300

    feat: config

---
+++ src/example/config/config.ts
@@ -0,0 +1,12 @@
+import { Constants } from '../../src'
+
+export const config = {
+  summonerName: 'iLenon7',
+  region: Constants.Regions.BRAZIL
+}
+
+export const configTft = {
+  summonerName: 'iLenon7',
+  region: Constants.Regions.BRAZIL,
+  tftRegion: Constants.TftRegions.AMERICAS
+}
